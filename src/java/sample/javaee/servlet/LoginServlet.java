9f89f872e6780f1b169374ad0e84507385ba840b
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-01-04 18:07:35 +0900

    form によるログイン機能を実装

---
+++ src/java/sample/javaee/servlet/LoginServlet.java
@@ -0,0 +1,22 @@
+package sample.javaee.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        
+        req.login(username, password);
+        
+        resp.sendRedirect("/servlet/secure/user/user.html");
+    }
+}
